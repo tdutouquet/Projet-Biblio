d44daaeb8a78457bd043437b41c011cd646a2e2a
Thomas Dutouquet <140429626+tdutouquet@users.noreply.github.com>
2024-04-17 17:20:35 +0200

    BO : home & user list

--- src/Controller/Admin/UserAdminController.php
+++ src/Controller/Admin/UserAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserAdminController extends AbstractController
 {
     #[Route('/admin/utilisateurs', name: 'app_user_admin')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
         return $this->render('admin/user_admin/index.html.twig', [
             'controller_name' => 'UserAdminController',
+            'users' => $users
         ]);
     }
 }
