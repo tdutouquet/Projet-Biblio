f7316de0bb407e38c19a5d8f24626f9323534cb5
Thomas Dutouquet <140429626+tdutouquet@users.noreply.github.com>
2024-04-17 20:04:41 +0200

    Account page & edit page WIP

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -11,8 +11,31 @@ class AccountController extends AbstractController
     #[Route('/compte', name: 'app_account')]
     public function index(): Response
     {
+        /** @var UserInterface $user */
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non connecté');
+        }
+
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+            'user' => $user
+        ]);
+    }
+
+    #[Route('/compte/modifier', name: 'app_account_edit')]
+    public function edit(): Response
+    {
+        /** @var UserInterface $user */
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non connecté');
+        }
+        return $this->render('account/edit.html.twig', [
+            'controller_name' => 'AccountEditController',
+            'user' => $user
         ]);
     }
 }
