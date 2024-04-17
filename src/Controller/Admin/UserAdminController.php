73983c7fa43f147d8da8719895637b765a2641b4
Thomas Dutouquet <140429626+tdutouquet@users.noreply.github.com>
2024-04-17 19:01:59 +0200

    BO : user details

--- src/Controller/Admin/UserAdminController.php
+++ src/Controller/Admin/UserAdminController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserAdminController extends AbstractController
 {
@@ -16,6 +18,17 @@ class UserAdminController extends AbstractController
         return $this->render('admin/user_admin/index.html.twig', [
             'controller_name' => 'UserAdminController',
             'users' => $users
+        ]);
+    }
+
+    #[Route('/admin/utilisateurs/{id}', name: 'app_user_admin_details')]
+    public function details(UserRepository $userRepository, int $id, Request $request, User $userDb): Response
+    {
+        $user = $userRepository->find($id);
+
+        return $this->render('admin/user_admin/details.html.twig', [
+            'controller_name' => 'UserAdminController',
+            'user' => $user,
         ]);
     }
 }
