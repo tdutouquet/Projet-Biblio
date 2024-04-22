bb8eed504569f414d06e0d9ce8d92a5195f21768
Thomas Dutouquet <140429626+tdutouquet@users.noreply.github.com>
2024-04-22 21:31:31 +0200

    Added password verif to register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -21,7 +21,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180)]
+    #[ORM\Column(length: 180, unique: true)]
     private ?string $email = null;
 
     /**
