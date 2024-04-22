616eec985166afc2b8957fcb55a99adb107d57c8
Beranger Priscylia <priscyliaberanger@gmail.com>
2024-04-22 11:47:23 +0200

    backoffice + Translate

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class MainController extends AbstractController
@@ -16,5 +17,19 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    #[Route('/changer-langue/{_locale}', name: 'changer_langue')]
+public function changerLangue(Request $request): Response
+{
+    // Récupérer la langue sélectionnée depuis la route
+    // $locale = $request->getLocale();
+    $locale = $request->get('_locale');
+
+    // Stocker la langue sélectionnée dans la session
+    $request->getSession()->set('_locale', $locale);
+
+    // Rediriger l'utilisateur vers la page précédente ou la page d'accueil
+    return $this->redirectToRoute('app_main');
+}
 
 }
